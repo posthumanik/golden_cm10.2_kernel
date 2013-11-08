2be60d8bb8ae3fef7c2bffbb9ea15f1df9152652
zwliew <zhaoweiliew@gmail.com>
2013-11-08 17:32:01 +0800

    lol nothing

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Nov 8 17:17:22 SGT 2013"
+#define LINUX_COMPILE_BY "maclaw"
+#define LINUX_COMPILE_HOST "maclaw.pl"
+#define LINUX_COMPILER "gcc version 4.8.2 20131014 (prerelease) (crosstool-NG hg+default-ca5924511405 - Linaro GCC 2013.10) "
